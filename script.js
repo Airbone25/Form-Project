dd4240a4824eaca455dd97c1da7f5e474a2c1bee
Keshav Mehra <keshavmehra2005@gmail.com>
2024-03-04 23:25:52 +0530

    Uploaded Files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+function login(){
+    const name = document.getElementById("name").value;
+    const pass = document.getElementById("pass").value;
+
+    if(name == "" || pass == ""){
+        alert("Fill Your Details!");
+    }
+}
